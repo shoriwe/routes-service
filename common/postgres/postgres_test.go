4e93eaab695108d10043f4a54cf09b007c3b77d0
Antonio <antoniojosedonishung@gmail.com>
2023-03-30 13:07:10 -0500

    fix: coverage support for connection to databases

--- common/postgres/postgres_test.go
+++ common/postgres/postgres_test.go
@@ -6,10 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewPostgreSQL(t *testing.T) {
-	dsn := "host=127.0.0.1 user=sulcud password=sulcud dbname=sulcud port=5432 sslmode=disable"
-	db := New(dsn)
-	conn, err := db.DB()
-	assert.Nil(t, err)
-	assert.Nil(t, conn.Close())
+func TestPostgres(t *testing.T) {
+	t.Run("ValidConnection", func(tt *testing.T) {
+		dsn := "host=127.0.0.1 user=routes password=routes dbname=routes port=5432 sslmode=disable"
+		db := New(dsn)
+		conn, err := db.DB()
+		assert.Nil(tt, err)
+		assert.Nil(tt, conn.Close())
+	})
+	t.Run("InvalidConnection", func(tt *testing.T) {
+		defer func() {
+			err := recover()
+			assert.NotNil(tt, err)
+		}()
+		New("")
+	})
 }
--- common/sqlite/sqlite_test.go
+++ common/sqlite/sqlite_test.go
@@ -6,9 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewSQLite(t *testing.T) {
-	db := NewTest()
-	conn, err := db.DB()
-	assert.Nil(t, err)
-	assert.Nil(t, conn.Close())
+func TestSQLite(t *testing.T) {
+	t.Run("ValidDSN", func(tt *testing.T) {
+		db := New(":memory:")
+		conn, err := db.DB()
+		assert.Nil(tt, err)
+		assert.Nil(tt, conn.Close())
+	})
+}
+
+func TestSQLite_NewTest(t *testing.T) {
+	t.Run("ValidDSN", func(tt *testing.T) {
+		db := NewTest()
+		conn, err := db.DB()
+		assert.Nil(tt, err)
+		assert.Nil(tt, conn.Close())
+	})
 }
