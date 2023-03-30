f4299ebef89662c818ac20126a62087737e49fa0
Antonio <antoniojosedonishung@gmail.com>
2023-03-30 14:20:26 -0500

    fix: fixed embedded testing map

--- maps/samples/samples.go
+++ maps/samples/samples.go
@@ -2,24 +2,24 @@ package samples
 
 import (
 	"encoding/json"
-	"os"
+
+	_ "embed"
 
 	"github.com/shoriwe/routes-service/maps"
 )
 
+//go:embed imaginary_city.json
+var ImaginaryCityContents []byte
+
 var ImaginaryCity maps.Map
 
-func loadMap(fileName string, target *maps.Map) {
-	file, oErr := os.Open("imaginary_city.json")
-	if oErr != nil {
-		panic(oErr)
-	}
-	uErr := json.NewDecoder(file).Decode(target)
+func loadMap(contents []byte, target *maps.Map) {
+	uErr := json.Unmarshal(contents, target)
 	if uErr != nil {
 		panic(uErr)
 	}
 }
 
 func init() {
-	loadMap("imaginary_city.json", &ImaginaryCity)
+	loadMap(ImaginaryCityContents, &ImaginaryCity)
 }
