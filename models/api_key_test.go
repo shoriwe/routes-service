a7e54c6e8189dd14b94108e1bd5acebe0bb23194
Antonio <antoniojosedonishung@gmail.com>
2023-03-30 13:24:44 -0500

    feat: API key model

---
+++ models/api_key_test.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/shoriwe/routes-service/common/sqlite"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPIKey(t *testing.T) {
+	db := sqlite.NewTest()
+	conn, _ := db.DB()
+	defer conn.Close()
+	assert.Nil(t, db.AutoMigrate(&Vehicle{}, &APIKey{}))
+	v := &Vehicle{
+		Name:  "Ford Mustang",
+		Plate: "COL000",
+	}
+	assert.Nil(t, db.Create(v).Error)
+	t.Run("ValidAPIKey", func(tt *testing.T) {
+		assert.Nil(tt, db.Create(&APIKey{
+			VehicleUUID: v.UUID,
+			Key:         "Test",
+		}).Error)
+	})
+}
