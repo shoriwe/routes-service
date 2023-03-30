297c04117421de7507a631c802c18689f9e31937
Antonio <antoniojosedonishung@gmail.com>
2023-03-30 12:10:39 -0500

    feat: Route model

---
+++ models/route.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type RouteStatus int
+
+const (
+	RouteInProgress RouteStatus = iota
+	RouteCancelled
+	RouteCompleted
+)
+
+type Route struct {
+	Model
+	VehicleUUID uuid.UUID   `gorm:"name:vehicle_uuid;not null;" json:"vehicle_uuid"`
+	Vehicle     Vehicle     `gorm:"foreignKey:vehicle_uuid;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
+	Started     time.Time   `gorm:"default:CURRENT_TIMESTAMP;not null;" json:"started"`
+	Completed   *time.Time  `json:"completed,omitempty"`
+	Status      RouteStatus `gorm:"not null;" json:"status"`
+	StartNode   int         `gorm:"not null;" json:"startNode"`
+	EndNode     int         `gorm:"not null;" json:"endNode"`
+}
